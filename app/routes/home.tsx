800f3ef71a390829ff9c41d3bb43ea634efedd49
joshua <zaitochegne@gmail.com>
2026-06-18 09:05:42 -0500

    delete welcome component because not is util

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,18 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "AI Aplication" },
+    { name: "description", content: "Welcome to AI App" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('images/bg-main.svg')] bg-cover">
+<h1>Welcome to AI Aplication </h1>
+<section>
+  <h2>Start your app here , not wait more </h2>
+</section>
+  </main>;
 }
