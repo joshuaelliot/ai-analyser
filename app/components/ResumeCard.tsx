ce7f2fb59ee0660a895055b58313a3898f351765
joshua <zaitochegne@gmail.com>
2026-06-23 13:24:46 -0500

    avanzando

--- app/components/ResumeCard.tsx
+++ app/components/ResumeCard.tsx
@@ -16,7 +16,12 @@ export default function ResumeCard({ resume }: { resume: Resume }) {
           <ScoreCircle score={resume.feedback.overallScore} />
         </div>
       </div>
-
+      <div className="gradient-border animate-in fade-in duration-1500">
+        <div className="w-full h-full">
+          <img src={resume.imagePath} alt={resume.companyName} 
+          className="w-full h-[350px] max-sm:h-[200px] object-cover object-top" />
+        </div>
+      </div>
     </Link>
   )
 }
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -15,12 +15,11 @@ export default function Home() {
   return <main className={"bg-[url('images/bg-main.svg')] bg-cover"}>
     <Navbar />
     <section className="main-section">
-      <div className="page-heading">
+      <div className="page-heading py-16">
         <h1>Welcome to AI Aplication </h1>
         <h2>Start your app here , not wait more </h2>
       </div>
-    </section>
-    {
+      {
       resumes.length > 0 && (
         <div className="resumes-section">
           {resumes.map((resume) => (
@@ -29,6 +28,8 @@ export default function Home() {
         </div>
       )
     }
+    </section>
+    
   </main>;
 }
 
